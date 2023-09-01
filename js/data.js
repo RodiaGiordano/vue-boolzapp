5136b04f5e5eea95478f309bf9193b40f0aa6a2d
RodiaGiordano <rodiagiordano93@gmail.com>
2023-09-01 16:08:02 +0200

    fix img

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@ const contacts = [
 
     {
         name: 'Michele',
-        avatar: './img/avatar_1.png',
+        avatar: './img/avatar_1.jpg',
         visible: true,
         messages: [
             {
@@ -29,7 +29,7 @@ const contacts = [
 
     {
         name: 'Fabio',
-        avatar: './img/avatar_2.png',
+        avatar: './img/avatar_2.jpg',
         visible: true,
         messages: [
             {
@@ -54,7 +54,7 @@ const contacts = [
 
     {
         name: 'Samuele',
-        avatar: './img/avatar_3.png',
+        avatar: './img/avatar_3.jpg',
         visible: true,
         messages: [
             {
@@ -79,7 +79,7 @@ const contacts = [
 
     {
         name: 'Alessandro B.',
-        avatar: './img/avatar_4.png',
+        avatar: './img/avatar_4.jpg',
         visible: true,
         messages: [
             {
@@ -98,7 +98,7 @@ const contacts = [
 
     {
         name: 'Alessandro L.',
-        avatar: './img/avatar_5.png',
+        avatar: './img/avatar_5.jpg',
         visible: true,
         messages: [
             {
@@ -117,7 +117,7 @@ const contacts = [
 
     {
         name: 'Claudia',
-        avatar: './img/avatar_5.png',
+        avatar: './img/avatar_6.jpg',
         visible: true,
         messages: [
             {
@@ -142,7 +142,7 @@ const contacts = [
 
     {
         name: 'Federico',
-        avatar: './img/avatar_7.png',
+        avatar: './img/avatar_7.jpg',
         visible: true,
         messages: [
             {
@@ -161,7 +161,7 @@ const contacts = [
     
     {
         name: 'Davide',
-        avatar: './img/avatar_8.png',
+        avatar: './img/avatar_8.jpg',
         visible: true,
         messages: [
             {
