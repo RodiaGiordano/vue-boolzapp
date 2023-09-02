ee36de2bac7268ffac19adbd2cb8b687b54c23de
RodiaGiordano <rodiagiordano93@gmail.com>
2023-09-02 19:13:50 +0200

    milestone 3 ok

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ createApp({
       contacts,
       
       activeChat: 0,
+
+      newMessage:"",
+    
     }   
   },
           
@@ -19,9 +22,40 @@ createApp({
     openChat(index){
       
       this.activeChat = index;
-      
-    }
+    },
     
+    
+    newMessageInput(activeChat, newMessage){
+  
+    
+
+      this.contacts[activeChat].messages.push(
+      {
+
+        date: '10:00',
+        message: this.newMessage,
+        status: 'received'
+
+      })
+
+      this.newMessage= '';
+
+      setTimeout(()=>
+        {
+          this.contacts[activeChat].messages.push({
+            date: '10:00',
+            message: 'ok',
+            status: 'sent'
+          })
+
+        },1000)
+    }
+
+
+
+
+
+
   }
   
   
